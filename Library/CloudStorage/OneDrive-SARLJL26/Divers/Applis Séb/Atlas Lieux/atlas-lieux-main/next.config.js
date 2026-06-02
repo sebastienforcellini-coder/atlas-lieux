5f047030f4cf2b0bb3ae5dc3734594dcc37f0081
sebastienforcellini-coder <268000556+sebastienforcellini-coder@users.noreply.github.com>
2026-06-02 15:24:25 +0200

    Nettoyage: retrait du dossier Library parasite (copie OneDrive dupliquee)

--- Library/CloudStorage/OneDrive-SARLJL26/Divers/Applis Séb/Atlas Lieux/atlas-lieux-main/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-})
-
-module.exports = withPWA({
-  reactStrictMode: true,
-})
